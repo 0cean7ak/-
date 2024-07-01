230ce5c263ccf42ff3ab13bdea874cfc5f791937
0cean7ak <akrm4839@naver.com>
2024-07-01 23:11:11 +0900

    web개발

---
+++ router.js
@@ -0,0 +1,29 @@
+
+const express = require('express')
+const router = express.Router();
+
+router.get('/list', (req,res)=>{
+
+})
+
+router.get('/wirte', (req,res)=>{
+    
+})
+
+router.get('/view', (req,res)=>{
+    
+})
+
+router.post('/update', (req,res)=>{
+    
+})
+
+router.get('/update', (req,res)=>{
+    
+})
+
+router.get('/delete', (req,res)=>{
+    
+})
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,12 @@
+
+const express = require('express')
+const boardRouter = require('./router.js')
+
+const app=express()
+
+app.use('/board',boardRouter)
+
+
+app.listen(3000,() => {
+    console.log('sever onload');
+})
